41f0ec4cef0138bf5309f7ed0d1d0dfd9e265885
yejiniee <02doyoung01@naver.com>
2023-03-21 16:56:08 +0900

    블로그 목록 뷰를 만들었다네

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Post
+
 
 # Create your views here.
+
+def index(request):
+    #사용자의 요청을 받아서 이런일 저런일을...
+    posts = Post.objects.all().order_by('-pk')
+
+    return render(
+        request,
+        'blog/index.html',
+        { #context
+            'posts': posts,
+        }
+    )

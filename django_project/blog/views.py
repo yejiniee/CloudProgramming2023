b249a61baf59a21b750b30a97b70b34ee187e099
yejiniee <02doyoung01@naver.com>
2023-03-28 15:39:46 +0900

    CBV refactoring

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,29 +1,42 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
 from .models import Post
 
 
 # Create your views here.
 
+'''
 def index(request):
     #사용자의 요청을 받아서 이런일 저런일을...
     posts = Post.objects.all().order_by('-pk')
 
     return render(
         request,
-        'blog/index.html',
+        'blog/post_list.html',
         {
             'posts': posts,
         }
     )
+'''
 
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+
+class PostDetail(DetailView):
+    model = Post
+
+'''
 def single_post_page(request, post_num):
 
     post = Post.objects.get(pk=post_num)
 
     return render(
         request,
-        'blog/single_post_page.html',
+        'blog/post_detail.html',
         {
             'post': post,
         }
     )
+'''

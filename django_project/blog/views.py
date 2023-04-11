0f9c29f36dae15f9906ea0e77e9eacf664ebb2ad
yejiniee <02doyoung01@naver.com>
2023-04-11 17:31:19 +0900

    add auth...

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,23 +1,50 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 from .models import Post, Category, Tag
 
 # Create your views here.
 
-'''
-def index(request):
-    #사용자의 요청을 받아서 이런일 저런일을...
-    posts = Post.objects.all().order_by('-pk')
+class PostUpdate(LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content', 'head_image', 'file_upload', 'category', 'tag']
 
-    return render(
-        request,
-        'blog/post_list.html',
-        {
-            'posts': posts,
-        }
-    )
-'''
+    template_name = 'blog/post_update.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user == self.get_object().author:
+            return super(PostUpdate, self).dispatch(request, *args, **kwargs)
+        else:
+            raise PermissionError
+
+    def get_context_data(self, **kwargs):
+        context = super(PostUpdate, self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        return context
+
+class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
+    model = Post
+    fields = ['title', 'content', 'head_image',  'file_upload', 'category', 'tag']
+
+    def test_func(self):
+        return self.request.user.is_superuser or self.request.user.is_staff
+
+    def form_valid(self, form):
+        current_user = self.request.user
+        if current_user.is_authenticated and (current_user.is_staff or current_user.is_superuser):
+            form.instance.author = current_user
+            return super(PostCreate, self).form_valid(form)
+        else:
+            redirect('/blog/')
+
+    def get_context_data(self, **kwargs):
+        context = super(PostCreate, self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        return context
+
 
 class PostList(ListView):
     model = Post
@@ -85,3 +112,5 @@ def tag_page(request, slug):
     }
 
     return render(request, 'blog/post_list.html', context)
+
+

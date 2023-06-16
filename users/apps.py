7632c0993ccfefe445fc83458ac216d1ba9800ee
yejiniee <02doyoung01@naver.com>
2023-06-17 01:15:10 +0900

    modify authentication

--- users/apps.py
+++ users/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
+
+'''
+#signals.py와 같이 삭제
+    def ready(self):
+        import users.signals
+'''
